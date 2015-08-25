2f0d6ac16f4c777a3134da8f6a1b06b4d7236503
Cobi Moos <cobimoos100@gmail.com>
2015-08-25 14:37:07 -0400

    copied the contact.rb file to this repo and added a require_relative statement for contact

--- crm.rb
+++ crm.rb
@@ -1,5 +1,15 @@
+require_relative 'contact'
 require 'sinatra'
 
 get '/' do
+  @crm_app_name = "My CRM"
   erb :index
-end"
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+get '/contacts/new' do
+  #add .erb later
+end
