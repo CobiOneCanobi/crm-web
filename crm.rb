886b79f581032d33dee483383b0fa2aec3a78190
Cobi Moos <cobimoos100@gmail.com>
2015-08-27 16:45:17 -0400

    centered form, added a footer, and more styling

--- crm.rb
+++ crm.rb
@@ -3,8 +3,6 @@ require_relative 'rolodex'
 require 'sinatra'
 
 $rolodex = Rolodex.new
-# Temporary fake data so that we always find contact with id 1000.
-$rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
 
 get '/' do
   @crm_app_name = "My CRM"
@@ -27,5 +25,42 @@ end
 
 get '/contacts/:id' do
   @contact = $rolodex.find(params[:id].to_i)
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get "/contacts/:id/edit" do
+    @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put "/contacts/:id" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete "/contacts/:id" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    $rolodex.remove_contact(@contact)
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
 end
